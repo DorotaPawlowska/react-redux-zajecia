a97dd154e12e688b2f400c0ebaa65656b9a07c7a
Dorota Pawłowska <dpawlowska@op.pl>
2018-06-17 15:37:42 +0200

    issue #1 counter

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,19 +1,33 @@
-import React, { Component } from 'react';
-import logo from '../logo.svg';
+import React, { Component, Fragment } from 'react';
 import '../App.css';
 
 class Home extends Component {
+
+    state = {
+        counter: 0,
+    };
+
+    handleCounterPlus = () => {
+        this.setState({
+            counter: this.state.counter + 1,
+        });
+    };
+
+    handleCounterMinus = () => {
+        this.setState({
+            counter: this.state.counter - 1,
+        });
+    };
+
+
     render() {
         return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h1 className="App-title">Welcome to React</h1>
-                </header>
-                <p className="App-intro">
-                    To get started, edit <code>src/App.js</code> and save to reload.
-                </p>
-            </div>
+            <Fragment>
+                <h1>{this.state.counter}</h1>
+                <button onClick={this.handleCounterPlus}> add one </button>
+                <hr/>
+                <button onClick={this.handleCounterMinus}> substract one </button>
+            </Fragment>
         );
     }
 }
