7c9dcaf15aa604eac55b5f2e56be03794c8f0e70
Dorota Pawłowska <dpawlowska@op.pl>
2018-06-17 17:06:35 +0200

    increment + decrement - zero

--- src/index.js
+++ src/index.js
@@ -1,11 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import './index.css';
-// import App from './App';
 import Home from './routes/Home';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<Home />, document.getElementById('root'));
+const reducer = ( state = 0, action) => {
+    switch (action.type){
+        case 'INCREMENT':
+            return state = state + 1;
+        case 'DECREMENT':
+            return state = state - 1;
+        case 'ZERO':
+            return state = 0;
+        default:
+            return state;
+    }
+};
+
+const store = createStore(reducer);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <Home />
+    </Provider>,
+    document.getElementById('root')
+);
 registerServiceWorker();
 
 
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,35 +1,33 @@
 import React, { Component, Fragment } from 'react';
+import { connect } from 'react-redux';
 import '../App.css';
 
 class Home extends Component {
 
-    state = {
-        counter: 0,
-    };
-
-    handleCounterPlus = () => {
-        this.setState({
-            counter: this.state.counter + 1,
-        });
-    };
-
-    handleCounterMinus = () => {
-        this.setState({
-            counter: this.state.counter - 1,
-        });
-    };
-
-
     render() {
         return (
             <Fragment>
-                <h1>{this.state.counter}</h1>
-                <button onClick={this.handleCounterPlus}> add one </button>
+                <h1>{this.props.counter}</h1>
+                <button onClick={this.props.increment}> add one </button>
                 <hr/>
-                <button onClick={this.handleCounterMinus}> substract one </button>
+                <button onClick={this.props.decrement}> substract one </button>
+                <hr/>
+                <button onClick={this.props.zeroing}> zero </button>
             </Fragment>
         );
     }
 }
 
-export default Home;
+const mapStateToProps = state => {
+    return { counter: state};
+};
+
+const mapDispachToProps = dispatch => {
+    return {
+        increment: () => dispatch({ type: 'INCREMENT'}),
+        decrement: () => dispatch({ type: 'DECREMENT'}),
+        zeroing: () => dispatch({ type: 'ZERO'})
+    };
+};
+
+export default connect (mapStateToProps, mapDispachToProps)(Home);
